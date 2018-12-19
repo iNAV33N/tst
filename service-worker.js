70827bb5af7f7d29dc959081dfc8dc3c089a017a
NAV33N <nav33n@live.in>
2018-12-19 08:26:14 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const cacheName = 'cache-v1';
+const precacheResources = [
+  '/',
+  'index.html',
+  'styles/style.css',
+  'images/icons/v128.png',
+  'images/icons/v144.png',
+  'images/icons/v152.png',
+  'images/icons/v192.png',
+  'images/icons/v256.png',
+  'scripts/min/logo.min.js',
+  'scripts/min/bg.min.js',
+  'scripts/min/app.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.0/css/bulma.min.css',
+  'https://use.fontawesome.com/releases/v5.0.7/js/all.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.debug.js'
+];
+
+self.addEventListener('install', event => {
+  console.log('Service worker install event!');
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(precacheResources);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activate event!');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+    );
+});
