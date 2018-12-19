1f11934fb8ba2b463954430332a1fa4e0b881f8a
NAV33N <nav33n@live.in>
2018-12-19 10:32:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,20 @@
 const cacheName = 'cache-v1';
 const precacheResources = [
   '/',
-  'index.html',
-  'styles/style.css',
-  'images/icons/v128.png',
-  'images/icons/v144.png',
-  'images/icons/v152.png',
-  'images/icons/v192.png',
-  'images/icons/v256.png',
-  'images/icons/v512.png',
-  'scripts/min/logo.min.js',
-  'scripts/min/bg.min.js',
-  'scripts/min/app.min.js',
+  '/index.html',
+  '/styles/style.css',
+  '/images/icons/v128.png',
+  '/images/icons/v144.png',
+  '/images/icons/v152.png',
+  '/images/icons/v192.png',
+  '/images/icons/v256.png',
+  '/images/icons/v512.png',
+  '/scripts/min/logo.min.js',
+  '/scripts/min/bg.min.js',
+  '/scripts/min/app.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.0/css/bulma.min.css',
   'https://use.fontawesome.com/releases/v5.0.7/js/all.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.debug.js'
-];
-
-const precacheResources2 = [
 ];
 
 self.addEventListener('install', event => {
@@ -25,7 +22,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => {
-        return cache.addAll(precacheResources2);
+        return cache.addAll(precacheResources);
       })
   );
 });
